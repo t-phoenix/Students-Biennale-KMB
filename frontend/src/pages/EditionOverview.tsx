a9f9cbe41d09ca872472cae47b01e0a17dc1e141
Vishnulalcr <crvishnulal@gmail.com>
2026-09-18 21:29:06 +0530

    fix(editions): align previous edition intro to span cols 4-9 from column 2 left edge to column 3 right edge

--- frontend/src/pages/EditionOverview.tsx
+++ frontend/src/pages/EditionOverview.tsx
@@ -584,11 +584,7 @@ function cleanIntroParagraphs(
             {subtitle}
           </h1>
         )}
-        <div
-          className={`${
-            isPreviousEdition ? "fig-c4-10" : "fig-c4-9"
-          } edition-overview__intro edition-overview__reveal`}
-        >
+        <div className="fig-c4-9 edition-overview__intro edition-overview__reveal">
           {intro.map((para, i) => (
             <p key={i} className="fig-body">
               <HighlightText text={para} query={highlight} />
